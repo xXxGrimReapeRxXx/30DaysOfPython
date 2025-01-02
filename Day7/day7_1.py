8129db930ccf89f0fd500a8c1137a170bd82fb08
Acko <ack0@abv.bg>
2025-01-02 21:25:26 +0200

    functions day7

---
+++ Day7/day7_1.py
@@ -0,0 +1,24 @@
+# It appears that the 30 day path that I
+# follow from github is broken and has certain parts that are missing
+# so I continue while learning on my own and following concepts exposed in the book
+# Automate the boring stuff 
+
+#   Functions
+
+
+
+# def hello ():
+#     print("how")
+#     print("Howdy!")
+#     print("hello there")
+
+
+
+# hello()
+# hello()
+
+
+def hello (name):
+    print("Hello," + name)
+
+hello("Ellie")
---
+++ Day7/day7_2.py
@@ -0,0 +1,11 @@
+# Keyword Arguments and the print() Function
+
+
+#  keyword arguments are identified by the keyword put before them in the function call.
+# Keyword arguments are often used for optional parameters.
+#  For example, the print() function has the optional parameters end and
+# sep to specify what should be printed at the end of its arguments and between its arguments (separating them), respectively.
+
+print('Hello', end='')
+print('World')
+print('cats', 'dogs', 'mice', sep=',')
